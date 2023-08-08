6e0ff3ef3321f8fbed92db8055ebf3c8f8dd1931
OneLean <leandro.montellano@gmail.com>
2023-08-08 14:37:25 -0300

    Configuración para render

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const path = require('path')
 
 app.use(express.static("public"));
 
-app.listen(8000,console.log('Listening on port 8000'));
+const port = process.env.PORT || 3001;
+app.listen(port,() => console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,"/views/home.html"));
